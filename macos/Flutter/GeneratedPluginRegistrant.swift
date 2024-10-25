06cbf3a38d5c6595c429a58a1debad595722b8e5
damlaYasarr <damlayasar40@gmail.com>
2024-10-25 20:06:14 +0300

    mobileapp

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import firebase_core
 import firebase_messaging
 import google_sign_in_ios
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
